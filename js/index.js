a3f7aa557997d33e97e22f562ed61472ff075aea
dosepehr <dosepehr@gmail.com>
2023-01-05 11:38:19 +0330

    make work tabs working

---
+++ js/index.js
@@ -0,0 +1,61 @@
+//!ham menu codes
+const ham = document.querySelector('.ham');
+const sidebar = document.querySelector('.sidebar');
+const closebtn = document.querySelector('.close');
+const body = document.querySelector('body');
+
+const sidebarHandler = () => {
+    sidebar.classList.toggle('active');
+};
+ham.onclick = (e) => {
+    e.stopPropagation();
+    sidebarHandler();
+    console.log(ham);
+};
+closebtn.onclick = () => {
+    sidebarHandler();
+};
+
+body.onscroll = () => {
+    if (sidebar.classList.contains('active')) {
+        sidebar.classList.remove('active');
+    }
+};
+
+//!select section codes
+const selectors = document.querySelectorAll('.work-select');
+const work_items = document.querySelectorAll('.work-item');
+console.log(selectors);
+selectors.forEach((selector) => {
+    selector.onclick = () => {
+        selectors.forEach((selector) => {
+            selector.classList.remove('select');
+        });
+
+        selector.classList.add('select');
+        if (selector.classList.contains('select')) {
+            work_items.forEach((work_item) => {
+                if (
+                    work_item.getAttribute('data-work') ==
+                    selector.getAttribute('data-select')
+                ) {
+                    work_item.classList.remove('unselected');
+                    setTimeout(() => {
+                        work_item.style.display = 'block';
+                    }, 500);
+                } else if (selector.getAttribute('data-select') == 'all') {
+                    work_item.classList.remove('unselected');
+                    setTimeout(() => {
+                        work_item.style.display = 'block';
+                    }, 500);
+                } else {
+                    work_item.classList.add('unselected');
+                    setTimeout(() => {
+                        work_item.style.display = 'none';
+                    }, 500);
+                }
+            });
+        }
+    };
+});
+
